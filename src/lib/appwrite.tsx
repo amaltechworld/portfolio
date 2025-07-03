f2672f484fb4d5c6b82643f77fb5ef9d55a028eb
Amal <amalrajsr19@gmail.com>
2025-07-03 21:51:57 +0530

    correction from vercel 3

--- src/lib/appwrite.tsx
+++ src/lib/appwrite.tsx
@@ -1,9 +1,13 @@
 import { Client, Databases, Account, Storage } from "appwrite";
 
 const client = new Client();
-client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+
+// Only initialize if environment variables are available
+if (process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT && process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID) {
+  client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+}
 
 const databases = new Databases(client);
 const account = new Account(client);
