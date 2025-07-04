e502a6b5cfa310fd61024706d388fb7d36559afb
Amal <amalrajsr19@gmail.com>
2025-07-04 11:47:15 +0530

    connection error from appwrite.tsx - solve

--- src/lib/appwrite.tsx
+++ src/lib/appwrite.tsx
@@ -3,7 +3,10 @@ import { Client, Databases, Account, Storage } from "appwrite";
 const client = new Client();
 
 // Only initialize if environment variables are available
-if (process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT && process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID) {
+if (
+  process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT &&
+  process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID
+) {
   client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
@@ -14,4 +17,3 @@ const account = new Account(client);
 const storage = new Storage(client);
 
 export { client, databases, account, storage };
- 
